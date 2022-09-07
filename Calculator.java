3dffe0cc26c9cbf2380249201656f45bc7eb39d0
Soare <alexandrusoare02@gmail.com>
2022-09-08 00:21:53 +0300

    -added functionality

--- Calculator.java
+++ Calculator.java
@@ -2,32 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator implements ActionListener{
+public class Calculator implements ActionListener {
 
 	JFrame frame;
 	JTextField textfield;
 	JButton[] numberButtons = new JButton[10];
 	JButton[] functionButtons = new JButton[9];
-	JButton addButton,subButton,mulButton,divButton;
+	JButton addButton, subButton, mulButton, divButton;
 	JButton decButton, equButton, delButton, clrButton, negButton;
 	JPanel panel;
-	
-	Font myFont = new Font("Ink Free",Font.BOLD,30);
-	
-	double num1=0,num2=0,result=0;
+
+	Font myFont = new Font("Tahoma", Font.BOLD, 30);
+
+	double num1 = 0, num2 = 0, result = 0;
 	char operator;
-	
-	Calculator(){
+
+	Calculator() {
 		frame = new JFrame("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 550);
 		frame.setLayout(null);
-		
+
 		textfield = new JTextField();
 		textfield.setBounds(50, 25, 300, 50);
 		textfield.setFont(myFont);
 		textfield.setEditable(false);
-		
+
 		addButton = new JButton("+");
 		subButton = new JButton("-");
 		mulButton = new JButton("*");
@@ -37,7 +37,7 @@ public class Calculator implements ActionListener{
 		delButton = new JButton("Del");
 		clrButton = new JButton("Clr");
 		negButton = new JButton("(-)");
-		
+
 		functionButtons[0] = addButton;
 		functionButtons[1] = subButton;
 		functionButtons[2] = mulButton;
@@ -47,27 +47,27 @@ public class Calculator implements ActionListener{
 		functionButtons[6] = delButton;
 		functionButtons[7] = clrButton;
 		functionButtons[8] = negButton;
-		
-		for(int i =0;i<9;i++) {
+
+		for (int i = 0; i < 9; i++) {
 			functionButtons[i].addActionListener(this);
 			functionButtons[i].setFont(myFont);
 			functionButtons[i].setFocusable(false);
 		}
-		
-		for(int i =0;i<10;i++) {
+
+		for (int i = 0; i < 10; i++) {
 			numberButtons[i] = new JButton(String.valueOf(i));
 			numberButtons[i].addActionListener(this);
 			numberButtons[i].setFont(myFont);
 			numberButtons[i].setFocusable(false);
 		}
-		
-		negButton.setBounds(50,430,100,50);
-		delButton.setBounds(150,430,100,50);
-		clrButton.setBounds(250,430,100,50);
-		
+
+		negButton.setBounds(50, 430, 100, 50);
+		delButton.setBounds(150, 430, 100, 50);
+		clrButton.setBounds(250, 430, 100, 50);
+
 		panel = new JPanel();
 		panel.setBounds(50, 100, 300, 300);
-		panel.setLayout(new GridLayout(4,4,10,10));
+		panel.setLayout(new GridLayout(4, 4, 10, 10));
 
 		panel.add(numberButtons[1]);
 		panel.add(numberButtons[2]);
@@ -85,30 +85,52 @@ public class Calculator implements ActionListener{
 		panel.add(numberButtons[0]);
 		panel.add(equButton);
 		panel.add(divButton);
-		
+
 		frame.add(panel);
 		frame.add(negButton);
 		frame.add(delButton);
 		frame.add(clrButton);
 		frame.add(textfield);
 		frame.setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Calculator calc = new Calculator();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		for(int i=0;i<10;i++) {
-			if(e.getSource() == numberButtons[i]) {
+
+		for (int i = 0; i < 10; i++) {
+			if (e.getSource() == numberButtons[i]) {
 				textfield.setText(textfield.getText().concat(String.valueOf(i)));
 			}
+			if (e.getSource() == decButton) {
+				textfield.setText(textfield.getText().concat("."));
+			}
+			if (e.getSource() == addButton) {
+				num1 = Double.parseDouble(textfield.getText());
+				operator = '+';
+				textfield.setText("");
+			}
+			if (e.getSource() == subButton) {
+				num1 = Double.parseDouble(textfield.getText());
+				operator = '-';
+				textfield.setText("");
+			}
+			if (e.getSource() == mulButton) {
+				num1 = Double.parseDouble(textfield.getText());
+				operator = '*';
+				textfield.setText("");
+			}
+			if (e.getSource() == divButton) {
+				num1 = Double.parseDouble(textfield.getText());
+				operator = '/';
+				textfield.setText("");
+			}
+			
 		}
 	}
 }
-	
-	
